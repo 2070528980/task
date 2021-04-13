a1a92ab161565dae84b62498bc33c6e3027378d4
wto <2070528980@qq.com>
2021-04-13 17:40:03 +0800

    0413

---
+++ app/js/setting.js
@@ -0,0 +1,18 @@
+// 公共配置
+
+var apiUrl = "http://127.0.0.1";
+var pro = window.location.protocol;
+
+function  getAPIUrl() {
+    if ('https:' == pro) {
+        apiUrl = pro + "//" + window.location.host + ":" + window.location.port + "/";
+    }
+    return apiUrl;
+
+}
+
+
+// 生成随机数
+function getRand(min,max) {
+    return Math.floor(Math.random()*(max-min))+min;
+}
