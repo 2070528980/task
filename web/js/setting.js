dedf9a798a931f89d9af1b7b8a132f1a489fe90a
wto <2070528980@qq.com>
2021-04-25 15:10:45 +0800

    0425

--- web/js/setting.js
+++ web/js/setting.js
@@ -6,8 +6,6 @@ layui.define(function (exports) {
         'api_url': 'http://94.191.115.200'
         // 'api_url': 'http://127.0.0.1'
         , 'img_url': 'http://94.191.115.200/file'
-
     };
-
     exports('setting', obj);
 });
